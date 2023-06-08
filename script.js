9b6c733755b974b95b0c51adb211e7589dbc4f70
valdineijunior <valdineidepaulajunior@gmail.com>
2023-06-07 22:09:42 -0300

    add questions and answers

--- script.js
+++ script.js
@@ -1,28 +1,29 @@
 (function () {
     const buttonCount = 8;
     const puzzle = [
-        ["mensagem 1"],
-        ["mensagem 2"],
-        ["mensagem 3"],
-        ["mensagem 4"],
-        ["mensagem 5"],
-        ["mensagem 6"],
-        ["mensagem 7"],
-        ["mensagem 8"],
-        ["mensagem 9"],
-        ["mensagem 10"],
+        ["Welcome ! To discover the first combination among the 80 buttons divided into 10 rows, I'm adding 1 clue in this text. Analyze carefully word by word to decrease the time you will need to solve the correct sequence of 4 buttons."],
+        ["Well, I think you understand a little bit about programming, am I right? If you give me the truth, you can proceed to the next puzzle."],
+        ["Very well, you solved the last puzzle easily, but unfortunately, you were not the first. At least 50 others came before you to get here. I wish you could have been the prime, but I'm sorry."],
+        ["Sometimes, the litle thing you're missing is the answer to the problem."],
+        ["Thank you for reaching challenge 5! You're doing great, keep up the good work."],
+        ["For this excellent work you are doing, you must be using good development tools, right ?"],
+        ["The two of us have been gaining experience in puzzles from the very beginning, doubling our capacity with each correct answer. How experienced are we now?"],
+        ["To unlock the next buttons below, which are currently in red, and turn them yellow, I will need a bit more intensity in this puzzle. Can you give me that ?"],
+        ["The next sequence can be found on the radio, depending on the base, It is the same from front to back and from top to bottom."],
+        ["Well, to close the puzzle, I would like to congratulate you. Your Odds are small, but just because of your effort to follow the guideLines, you finished it."],
+        ["Congratulations on solving those puzzles!"]
     ];
     const combinations = [
+        [0, 0, 0, 0, 0, 1, 0, 1],
         [0, 0, 0, 0, 0, 0, 0, 1],
-        [0, 1, 1, 0, 0, 1, 1, 0],
-        [0, 1, 1, 0, 0, 1, 1, 0],
-        [0, 1, 1, 0, 0, 1, 1, 0],
-        [0, 1, 1, 0, 0, 1, 1, 0],
-        [0, 1, 1, 0, 0, 1, 1, 0],
-        [0, 1, 1, 0, 0, 1, 1, 0],
-        [0, 1, 1, 0, 0, 1, 1, 0],
-        [0, 1, 1, 0, 0, 1, 1, 0],
-        [0, 1, 1, 0, 0, 1, 1, 0],
+        [1, 1, 1, 0, 1, 0, 0, 1],
+        [0, 1, 1, 1, 0, 1, 0, 0],
+        [0, 1, 1, 1, 1, 0, 0, 0],
+        [1, 0, 0, 1, 1, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0],
+        [1, 1, 1, 1, 1, 1, 1, 1],
+        [0, 1, 0, 1, 1, 0, 0, 0],
+        [1, 0, 1, 0, 0, 0, 0, 0],
     ];
 
     let nextCombinationIndex = 0;
@@ -88,15 +89,14 @@
             currentCombination.every((state, index) => state === buttonStates[index]);
 
         if (isCorrectCombination) {
-            console.log("Combinação correta!");
-
             nextCombinationIndex++;
-
+            if(nextCombinationIndex == 5) {
+              console.log("GOODTOOL")
+            }
             removeLockedElements();
             addCompletedElements();
             addEventListenerToNewButtons();
         } else {
-            console.log("Combinação incorreta!");
         }
     }
 
